63c2a76245534a51b41088655e102df25d8354b8
Ian Hwang <ianhwang322@gmail.com>
2025-05-17 10:12:55 -0500

    add custom item and creative mod tab

--- src/main/java/net/ianhwang/tutorialmod/TutorialMod.java
+++ src/main/java/net/ianhwang/tutorialmod/TutorialMod.java
@@ -1,7 +1,10 @@
 package net.ianhwang.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ianhwang.tutorialmod.item.ModCreativeModeTabs;
+import net.ianhwang.tutorialmod.item.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,6 +31,8 @@ public class TutorialMod
     public TutorialMod(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
+        ModCreativeModeTabs.register(modEventBus);
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -50,7 +55,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
